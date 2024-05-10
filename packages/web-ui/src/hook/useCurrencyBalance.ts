3f77c546ea39e03b1b44f983ac37c19ef26767a9
AeolusZane <1393649219@qq.com>
2024-05-10 17:48:45 +0800

    supply commit file "rewrite contract interaction"

--- packages/web-ui/src/hook/useCurrencyBalance.ts
+++ packages/web-ui/src/hook/useCurrencyBalance.ts
@@ -2,22 +2,20 @@ import { useWeb3React } from '@web3-react/core'
 import { useEffect, useState } from 'react'
 import Web3 from 'web3';
 
-
 export function useCurrencyBalance() {
-    const { accounts, provider } = useWeb3React();
+    const { account, provider } = useWeb3React();
     const [balance, setBalance] = useState<string | null>('-');
-
     useEffect(() => {
-        if (provider && accounts && accounts.length > 0) {
-            provider.getBalance(accounts[0]).then((balance) => {
+        if (provider && account) {
+            provider.getBalance(account).then((balance) => {
                 setBalance(Web3.utils.fromWei(balance.toString(), 'ether'))
             })
         }
     }, [provider]);
 
-    if (!accounts || accounts.length === 0) {
-        console.log('No accounts found')
+    if (!account) {
+        // console.log('No accounts found')
     }
 
-    return { account: accounts?.[0] || '-', balance }
+    return { account: account || '-', balance }
 }
