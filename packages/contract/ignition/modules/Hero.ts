eb7f3394bed7c74f55069ef63c0625bcca7e7045
AeolusZane <1393649219@qq.com>
2024-05-12 21:10:10 +0800

    differentiate production & development when ignition

--- packages/contract/ignition/modules/Hero.ts
+++ packages/contract/ignition/modules/Hero.ts
@@ -1,10 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-const FOUNDER_ADDRESS = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 const HeroModule = buildModule("Hero", (m) => {
+    const constructor = process.env.NODE_ENV === 'development' ?
+        process.env.TEST_NET_ADDRESS :
+        process.env.ADDRESS;
+        
     const hero = m.contract("Hero", [
-        FOUNDER_ADDRESS
+        constructor!
     ]);
     return { hero }
 });
