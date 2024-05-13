01f45fd518b51dbba2d7418aba59d82ca3f8bf0a
AeolusZane <1393649219@qq.com>
2024-05-13 22:05:33 +0800

    add EIP-3085 to connect with a new chain

--- packages/web-ui/src/constants/chain.ts
+++ packages/web-ui/src/constants/chain.ts
@@ -11,16 +11,35 @@ export const CHAIN_NAMES = {
     [MAIN_NETS.LOCAL_TEST]: 'Localhost',
     [MAIN_NETS.ALCHEMY]: 'Sepolia'
 }
+export const PUBLIC_RPC_URLS = {
+    [MAIN_NETS.ETHER]: ['https://mainnet.infura.io/v3/'],
+    [MAIN_NETS.LOCAL_TEST]: ['http://localhost:8545'],
+    [MAIN_NETS.ALCHEMY]: ['']
 
+}
 const CHAIN_INFO_MAP = {
     [MAIN_NETS.ETHER]: {
-        label: CHAIN_NAMES[MAIN_NETS.ETHER]
+        label: CHAIN_NAMES[MAIN_NETS.ETHER],
+        chainId: MAIN_NETS.ETHER,
+        name: 'Ethereum',
+        rpcUrl: 'https://mainnet.infura.io/v3/',
+        nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     },
     [MAIN_NETS.LOCAL_TEST]: {
-        label: CHAIN_NAMES[MAIN_NETS.LOCAL_TEST]
+        label: CHAIN_NAMES[MAIN_NETS.LOCAL_TEST],
+        chainId: MAIN_NETS.LOCAL_TEST,
+        name: CHAIN_NAMES[MAIN_NETS.LOCAL_TEST],
+        rpcUrl: 'http://localhost:8545',
+        nativeCurrency: { name: 'Local Ether', symbol: 'ETH', decimals: 18 },
+
     },
     [MAIN_NETS.ALCHEMY]: {
-        label: CHAIN_NAMES[MAIN_NETS.ALCHEMY]
+        label: CHAIN_NAMES[MAIN_NETS.ALCHEMY],
+        chainId: MAIN_NETS.ALCHEMY,
+        name: CHAIN_NAMES[MAIN_NETS.ALCHEMY],
+        rpcUrl: '',
+        nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+        // get rpcUrl on alchemy
     }
 
 }
--- packages/web-ui/src/hook/useSelectChain.ts
+++ packages/web-ui/src/hook/useSelectChain.ts
@@ -1,10 +1,20 @@
 import { useWeb3React } from "@web3-react/core";
+import { getChainInfo, PUBLIC_RPC_URLS, MAIN_NETS } from "@constants/chain";
 
 export function useSelectChain() {
     const { connector } = useWeb3React();
 
-    const switchChain = async (chainId: number) => {
-        await connector.activate({ chainId });
+    const switchChain = async (chainId: MAIN_NETS) => {
+        const chainInfo = getChainInfo(chainId);
+        const addChainParameter = {
+            chainId: chainInfo.chainId,
+            chainName: chainInfo.name,
+            rpcUrls: [PUBLIC_RPC_URLS[chainId][0]],
+            nativeCurrency: chainInfo.nativeCurrency
+        }
+        await connector.activate(addChainParameter)
+
+        // await connector.activate({ chainId });
     };
 
     return switchChain
